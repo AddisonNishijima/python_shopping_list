d0224ab770f83a6a12b9d3db06c9b5013364ac9b
Addison Nishijima <addison.nishijima@gmail.com>
2017-04-14 12:15:02 -0700

    refactor shopping_list

--- shopping_list.py
+++ shopping_list.py
@@ -1,52 +1,40 @@
 shopping_list = []
 
-def remove_item(idx):
-	index = idx - 1
-	item = shopping_list.pop(index)
-	print("Remove {}.".format(item))
-
 
 def show_help():
-	print("\nSeparate each item with a comma.")
-	print("Type DONE to quit, SHOW to see the current list, REMOVE to delete an item, and HELP to get this message.")
-	
+    print("What should we pick up at the store?")
+    print("""
+Enter 'DONE' to stop adding items.
+Enter 'HELP' for this help.
+Enter 'SHOW' to see your current list.
+""")
+
+
+def add_to_list(item):
+    shopping_list.append(new_item)
+    print("Added! List has {} items.".format(len(shopping_list)))
+
 
 def show_list():
-	count = 1
-	for item in shopping_list:
-		print("{}: {}".format(count, item))
-		count += 1
-			  
-print("Give me a list of things you want to shop for.")
+    print("Here's your list:")
+    for item in shopping_list:
+        print(item)
+
+
 show_help()
-			  
+
 while True:
-	new_stuff = input("> ")
-	
-	if new_stuff == "DONE":
-		print("\nHere's your list:")
-		show_(list)
-		break
-	elif new_stuff == "HELP":
-		show_help()
-		continue
-	elif new_stuff == "SHOW":
-		show_list()
-		continue
-	elif new_stuff == "REMOVE":
-		show_list()
-		idx = input("Which item? Tell me the number. ")
-		remove_item(int(idx))
-		continue
-	else:
-		new_list = new_stuff.split(",")
-		index = input(" Add this at a certain spot? Press enter for the end of the list, or give me a number. Current {} items in the list.".format(len(shopping_list)))
-		
-		if index:
-			spot = int(index) - 1
-			for item in new_list:
-				shopping_list.insert(spot,item.strip())
-				spot += 1
-		else:
-			for item in new_list:
-				shopping_list.append(item.strip())
+    new_item = input("> ")
+
+    if new_item == 'DONE':
+        break
+    elif new_item == 'HELP':
+        show_help()
+        continue
+    elif new_item == 'SHOW':
+        show_list()
+        continue
+
+    add_to_list(new_item)
+
+show_list()
