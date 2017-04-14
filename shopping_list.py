3b7c222ec9b587eaf42b9554d83b4dfe2bb62848
Addison Nishijima <addison.nishijima@gmail.com>
2017-04-14 12:49:37 -0700

    fix indentation and spacing

--- shopping_list.py
+++ shopping_list.py
@@ -2,11 +2,13 @@ import os
 
 shopping_list = []
 
+
 def clear_screen():
-	os.system("cls" if os.name == "nt" else "clear")
+  os.system("cls" if os.name == "nt" else "clear")
+
 
 def show_help():
-	clear_screen()
+    clear_screen()
     print("What should we pick up at the store?")
     print("""
 Enter 'DONE' to stop adding items.
@@ -16,20 +18,37 @@ Enter 'SHOW' to see your current list.
 
 
 def add_to_list(item):
-    shopping_list.append(new_item)
-    print("Added! List has {} items.".format(len(shopping_list)))
-	show_list()
+    show_list()
+    if len(shopping_list):
+        position = input("Where should I add {}?\n"
+                         "Press ENTER to add to the end of the list\n"
+                         "> ".format(item))
+    else:
+        position = 0
+
+    try:
+        position = abs(int(position))
+    except ValueError:
+        position = None
+    if position is not None:
+        shopping_list.insert(position-1, item)
+    else:
+        shopping_list.append(new_item)
+
+    show_list()
 
 
 def show_list():
-	clear_screen()
+    clear_screen()
 
     print("Here's your list:")
 
-	index = 1
+    index = 1
     for item in shopping_list:
         print("{}. {}".format(index, item))
-		index += 1
+        index += 1
+
+    print("-"*10)
 
 
 show_help()
@@ -45,7 +64,7 @@ while True:
     elif new_item.upper() == 'SHOW':
         show_list()
         continue
-	else:
-    add_to_list(new_item)
+    else:
+        add_to_list(new_item)
 
 show_list()
